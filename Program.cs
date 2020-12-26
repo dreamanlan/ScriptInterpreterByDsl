18aa3f15b5f2fe4b6d50845d085c08802269f0b6
dreamanlan <dreaman_163@163.com>
2020-12-26 23:41:39 +0800

    添加等待与读按键API，示例脚本加上循环

--- Program.cs
+++ Program.cs
@@ -18,13 +18,19 @@ namespace TestDsl
 {
     echo('hello world !');
     $a=1;
-    $b=2;
-    if($a<$b){
-        echo('LT');
-    }
-    else{
-        echo('GE');
+    $b=20;
+    while($a<50){
+        if($a<$b){
+            echo('{0} LT {1}', $a, $b);
+        }
+        else{
+            echo('{0} GE {1}', $a, $b);
+        };
+        $a = $a + 1;
+        wait(100);
     };
+    echo('press any key to exit ...');
+    readkey();
 };";
             Execute(script);
         }
@@ -33,6 +39,8 @@ namespace TestDsl
         {
             Interpreter interpreter = new Interpreter();
             interpreter.Register("echo", new ExpressionFactoryHelper<EchoExp>());
+            interpreter.Register("wait", new ExpressionFactoryHelper<WaitExp>());
+            interpreter.Register("readkey", new ExpressionFactoryHelper<ReadKeyExp>());
             if (interpreter.Parse(code, "testscript")) {
                 object v = interpreter.Call("main");
                 if (null == v) {
@@ -63,6 +71,26 @@ namespace TestDsl
                 }
             }
             return false;
+        }
+    }
+    class WaitExp : SimpleExpressionBase
+    {
+        protected override object OnCalc(IList<object> operands)
+        {
+            if (operands.Count == 1) {
+                int ms = (int)Convert.ChangeType(operands[0], typeof(int));
+                System.Threading.Thread.Sleep(ms);
+                return true;
+            }
+            return false;
+        }
+    }
+    class ReadKeyExp : SimpleExpressionBase
+    {
+        protected override object OnCalc(IList<object> operands)
+        {
+            var info = Console.ReadKey();
+            return info.KeyChar;
         }
     }
 }
